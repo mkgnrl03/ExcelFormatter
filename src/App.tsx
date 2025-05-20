eff753c9a03a3e15eeb12160fccdb3b48aa5f9e3
mkgnrl <mikognrl@gmail.com>
2025-05-20 11:37:14 +0800

    removed unused declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import type { ChangeEvent } from 'react';
 import * as XLSX from 'xlsx';
 import { Loader } from 'lucide-react';
